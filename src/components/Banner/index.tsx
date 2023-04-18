c653dc32044f5a95ba412bb171c899aa2ee5551f
Brahian Sanchez <brahian.sanchez@experimentality.co>
2023-04-17 20:56:30 -0500

    [Update] Work 🏗️: PWA and Banner component

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,15 @@
+// Image component
+import Image from 'next/image'
+
+// Interface
+import { TBanner } from '../../interfaces/Banner/banner.interface'
+
+const Banner = ({ image }: TBanner): JSX.Element => (
+  <>
+    <div className='flex justify-center'>
+      <p> {image} </p>
+    </div>
+  </>
+)
+
+export default Banner
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
+// Import banner component
+import Banner from '../components/Banner/index'
+
 export default function Home (): JSX.Element {
   return (
-    <p> Hello word from Next </p>
+    <>
+      <Banner image='Imagen interesante !' />
+    </>
   )
 }
