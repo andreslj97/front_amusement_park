f6a476848e72e79952268dc74b8d5dd893f7a201
Roniber Andrés Lugo Jiménez <roniber138@hotmail.com>
2023-05-15 00:59:08 -0500

    Adjust deploy

--- src/components/Steps/StepPayment.tsx
+++ src/components/Steps/StepPayment.tsx
@@ -15,14 +15,14 @@ const StepPayment = ({ setStep, step }: StepperProps): JSX.Element => {
           <form className='w-full max-w-lg'>
             <div className='flex flex-wrap -mx-3 mb-6'>
               <div className='w-full md:w-1/2 px-3 mb-6 md:mb-0'>
-                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' for='grid-first-name'>
+                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>
                   Nombres
                 </label>
                 <input className='appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white' id='grid-first-name' type='text' placeholder='' />
                 <p className='text-red-500 text-xs italic'>Completa el campo.</p>
               </div>
               <div className='w-full md:w-1/2 px-3'>
-                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' for='grid-last-name'>
+                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>
                   Apellidos
                 </label>
                 <input className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500' id='grid-last-name' type='text' placeholder='' />
@@ -30,14 +30,14 @@ const StepPayment = ({ setStep, step }: StepperProps): JSX.Element => {
             </div>
             <div className='flex flex-wrap -mx-3 mb-6'>
               <div className='w-full md:w-1/2 px-3 mb-6 md:mb-0'>
-                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' for='grid-city'>
+                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>
                   Ciudad
                 </label>
                 <input className='appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white' id='grid-first-name' type='text' placeholder='' />
                 <p className='text-red-500 text-xs italic'>Completa el campo.</p>
               </div>
               <div className='w-full md:w-1/2 px-3'>
-                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' for='grid-state'>
+                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>
                   Tipo de identificación
                 </label>
                 <div className='relative'>
@@ -53,14 +53,14 @@ const StepPayment = ({ setStep, step }: StepperProps): JSX.Element => {
             </div>
             <div className='flex flex-wrap -mx-3 mb-6'>
               <div className='w-full md:w-1/2 px-3 mb-6 md:mb-0'>
-                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' for='grid-first-name'>
+                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>
                   Email
                 </label>
                 <input className='appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white' id='grid-first-name' type='text' placeholder='' />
                 <p className='text-red-500 text-xs italic'>Completa el campo.</p>
               </div>
               <div className='w-full md:w-1/2 px-3'>
-                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' for='grid-last-name'>
+                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>
                   Celular
                 </label>
                 <input className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500' id='grid-last-name' type='text' placeholder='' />
